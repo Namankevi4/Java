93c9e773b84b1276fc3c1c4c2b9d539680149997
Namankevi4 <namankevi4@gmail.com>
2015-02-23 20:43:47 +0300

    Read-write

---
+++ read_write.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.Scanner;
+public class read_write
+{
+	public static void main(String[] args)
+	{
+         try(Scanner bf = new Scanner (new FileReader(args[0])))
+         {
+        	 int numb_of_string=0;
+        	 int count =0;
+        	 try
+        	 {
+        	 numb_of_string = Integer.parseInt(args[1]);
+        	 count = Integer.parseInt(args[2]);
+        	 } catch (Exception e) {System.out.println("Incorrect input");}
+	         
+           int c =1;
+	       while (bf.hasNext() && c<numb_of_string)
+           {
+	    	  bf.nextLine();
+        	  c++;
+           }
+	       
+	       while (bf.hasNext() && c<numb_of_string+count )
+	       {
+	    	   System.out.println(bf.nextLine());
+	    	   c++;
+	       }
+         } catch (Exception e) {System.out.println("File not Found");} 
+	
+     }
+}
